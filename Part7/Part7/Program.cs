1c217dcc4b98bf47d24c0b5e7be9a753336feacc
Alexander Eklöf <alexander.eklof@iths.se>
2024-08-28 14:15:48 +0200

    Uppgift

---
+++ Part7/Part7/Program.cs
@@ -0,0 +1,19 @@
+﻿//7. Discount System:
+
+Console.WriteLine("Enter a price: ");
+string input = Console.ReadLine();
+
+
+if (int.TryParse(input, out int price))
+{
+    if (price >= 1000)
+    {
+        Console.WriteLine("The price is " + (price - (price * 0.1)));
+    }
+    else
+    {
+        Console.WriteLine("The price is " + price);
+    }
+
+
+}
