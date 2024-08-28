f41b7e4c1563e1c4b76ba74441671caee1815e2c
Alexander Eklöf <alexander.eklof@iths.se>
2024-08-28 13:54:00 +0200

    Upggift

---
+++ Part2/Part2/Program.cs
@@ -0,0 +1,20 @@
+﻿//2. Age Bracket:
+
+Console.WriteLine("Enter your age: ");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int age))
+{
+    if (age < 13)
+    {
+        Console.WriteLine("You are a child");
+    }
+    else if (age >= 13 && age < 19)
+    {
+        Console.WriteLine("You are a teenager");
+    }
+    else if (age > 19)
+    {
+        Console.WriteLine("You are an adult");
+    }
+}
