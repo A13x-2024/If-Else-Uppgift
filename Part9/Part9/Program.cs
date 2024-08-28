a1ed3a1d3eee85db5666ee9b0316af5f86e8fe55
Alexander Eklöf <alexander.eklof@iths.se>
2024-08-28 14:18:22 +0200

    Uppgift

---
+++ Part9/Part9/Program.cs
@@ -0,0 +1,16 @@
+﻿//9. Password Checker:
+
+
+Console.WriteLine("Enter your password: ");
+string input = Console.ReadLine();
+
+string predefinedPassword = "CSharp123";
+
+if (input == predefinedPassword)
+{
+    Console.WriteLine("Access granted");
+}
+else
+{
+    Console.WriteLine("Access denied");
+}
