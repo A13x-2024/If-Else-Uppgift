7730fb042c1d7a5b00ac3b0e68cd2c379fa96de0
Alexander Eklöf <alexander.eklof@iths.se>
2024-08-28 14:14:20 +0200

    Uppgift

---
+++ Part6/Part6/Program.cs
@@ -0,0 +1,20 @@
+﻿//6. Positive, Negative, or Zero:
+
+Console.WriteLine("Enter a number: ");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int number))
+{
+    if (number > 0)
+    {
+        Console.WriteLine("The number is positive");
+    }
+    else if (number < 0)
+    {
+        Console.WriteLine("The number is negative");
+    }
+    else
+    {
+        Console.WriteLine("The number is zero");
+    }
+}
