38256db2c40b3b8f6a0316101b809f780371bd1c
Alexander Eklöf <alexander.eklof@iths.se>
2024-08-28 14:17:13 +0200

    Uppgift

---
+++ Part8/Part8/Program.cs
@@ -0,0 +1,18 @@
+﻿//8. Weekdays:
+
+Console.WriteLine("Enter a number between 1 and 7: ");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int day))
+{
+    if (day >= 1 && day <= 7)
+    {
+        string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        Console.WriteLine("It's " + daysOfWeek[day - 1]);
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a number between 1 and 7.");
+    }
+
+}
