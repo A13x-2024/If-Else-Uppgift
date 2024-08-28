d7d1a695b8175a52b753b8e34814133dd68dcde9
Alexander Eklöf <alexander.eklof@iths.se>
2024-08-28 13:27:19 +0200

    Add project files.

---
+++ If-Else-Part1/If-Else-Part1/Program.cs
@@ -0,0 +1,23 @@
+﻿//1. Even or Odd:
+
+using System;
+
+Console.WriteLine("Enter a number: ");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int number))
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("The number is even");
+    }
+    else
+    {
+        Console.WriteLine("The number is odd");
+    }
+
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
